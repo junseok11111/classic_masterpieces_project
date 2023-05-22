d8b4b6c5ab4536b9d82bfaf8ab635987777631eb
junseok <pepaa15@naver.com>
2023-05-22 22:03:41 +0900

    Junseok commit 0522

---
+++ 0522-2/front-server/src/views/ArticleView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <button @click.prevent="createArticle">글쓰기</button>
+    <ArticleItem
+      v-for="article in articles"
+      :key="article.id"
+      :article="article"
+    />
+  </div>
+</template>
+
+<script>
+import ArticleItem from '@/components/ArticleItem.vue'
+
+import {mapState} from 'vuex'
+
+export default {
+  name: 'ArticleView',
+  components: {
+    ArticleItem,
+  },
+  computed: {
+    ...mapState({
+      articles: state => state.articleModule.articles
+    }),
+  },
+  methods: {
+    getArticles() {
+      this.$store.dispatch('getArticles')
+    },
+    createArticle() {
+      
+    }
+  },
+  created() {
+    this.getArticles()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
