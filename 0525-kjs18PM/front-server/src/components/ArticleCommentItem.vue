b9163e37420c9f9162640cf071ae0215fb8f6538
junseok <pepaa15@naver.com>
2023-05-25 17:54:02 +0900

    Junseok Commit 6PM

---
+++ 0525-kjs18PM/front-server/src/components/ArticleCommentItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{comment.content}} / 작성자: {{comment.user_name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleCommentItem',
+  props: {
+    comment: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 0525-kjs18PM/front-server/src/components/GenreActionCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <router-link :to="{name:'MovieDetailView', params: {id: movie.id}}" class="genre-movie-card">
+      <div><img :src="`http://image.tmdb.org/t/p/w500${movie?.poster_path}`" alt="..." class="Detail-Card-img" ></div>
+      <div class="Detail-Card-Title"><p>{{movieTitle}}</p></div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GenreActionCard',
+  props: {
+    movie: Object,
+  },
+  computed: {
+    movieTitle() {
+      return this.movie?.title
+    },
+  }
+}
+</script>
+
+<style>
+.Detail-Card-Title{
+  color: white;
+}
+.Detail-Card-img{
+  width: 200px;
+}
+.genre-movie-card {
+  text-decoration: none;
+}
+</style>
